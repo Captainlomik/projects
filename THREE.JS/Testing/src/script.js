f0904e25d702fd534ec75a98fa40631ea2719643
CaptainLomik <nastiy248@mail.ru>
2021-04-27 22:04:17 +0300

    done

--- THREE.JS/Testing/src/script.js
+++ THREE.JS/Testing/src/script.js
@@ -1,13 +1,18 @@
-import { appendFile } from "fs"
+import { appendFile} from "fs"
 import fragment from "./shaders/fragment.glsl"
-import vertex from  "./shaders/vertex.glsl"
+import fragment1 from "./shaders/fragment1.glsl"
+import vertex from "./shaders/vertex.glsl"
 import "./style.scss"
 import * as THREE from 'three'
 import * as dat from 'dat.gui'
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
-import { DoubleSide } from "three"
-import CANNON from 'cannon'
+import {OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import landscape from './img/bg.jpg'
+import {EffectComposer} from 'three/examples/jsm/postprocessing/EffectComposer.js'
+import {RenderPass} from 'three/examples/jsm/postprocessing/RenderPass'
+import {ShaderPass} from 'three/examples/jsm/postprocessing/ShaderPass'
+import {PostProcessing} from './postprocessing'
+
+
 
 /**
  * Base
@@ -15,111 +20,99 @@ import landscape from './img/bg.jpg'
 //Canvas
 const canvas = document.querySelector('.canvasModel')
 
+
 //Scene 
 const scene = new THREE.Scene()
 
 //GUI
-const gui = new dat.GUI({width:340})
+const gui = new dat.GUI({
+    width: 340
+})
+
 
 /**
- * PHYSYCS
+ * Mouse 
  */
+let mouse = 0
+let lastX = 0
+let lastY = 0
+let speed = 0
+
+document.addEventListener('mousemove', (e)=>{
+    speed = Math.sqrt((e.pageX - lastX)**2 +(e.pageY - lastY)**2) *0.1;
+    lastX = e.pageX;
+    lastY = e.pageY;
+})
 
 //Material
 
-//material = new THREE.MeshNormalMaterial()
-let t =  new THREE.TextureLoader().load(landscape)
+let t = new THREE.TextureLoader().load(landscape)
 t.wrapS = t.wrapT = THREE.MirroredRepeatWrapping
 
- const material = new THREE.ShaderMaterial({
-    side:THREE.DoubleSide,
-    fragmentShader:fragment,
-    vertexShader:vertex,
-    uniforms:{
-        process:{type:"f", value:0},
-        landscape:{value: t}
+const material = new THREE.ShaderMaterial({
+    side: THREE.DoubleSide,
+    extensions:{
+        derivatives:"#extension GL_OES_standard_derivatives : enable"
+    },
+    fragmentShader: fragment,
+    vertexShader: vertex,
+    uniforms: {
+        time: {
+            type: "f",
+            value: 0
+        },
+        mouse: {
+            value: 0
+        },
+        landscape: {
+            value: t
+        },
+        resolution:{type:"v4", value: new THREE.Vector4()},
+        uvRate1:{
+            value: new THREE.Vector2(1,1)
+        }
     },
     //wireframe:true
 })
 
-
-// const defaultMaterial =  new CANNON.Material('default')
-// const softMaterial = new CANNON.Material('soft', -1)
-// const defaultContactMaterial = new CANNON.ContactMaterial(
-//     softMaterial, 
-//     defaultMaterial,{
-//         friction:1, 
-//         restitution:1
-//     }
-// )
-
-// const depthMaterial = new THREE.MeshDepthMaterial({
-//     depthPacking:THREE.RGBADepthPacking
-// })
-
-
-/**
- * Cube
- */
-// const cube = new THREE.Mesh(
-//     new THREE.SphereBufferGeometry(0.5, 32, 32),
-//     new THREE.MeshStandardMaterial({
-//         metalness:0.3, 
-//         roughness:0.4
-//     })
-// )
-// cube.position.y = 0.5
-// scene.add(cube)
-
-//Floor
-// const floor = new THREE.Mesh(
-//     new THREE.PlaneBufferGeometry(6,6),
-//     new THREE.MeshStandardMaterial({
-//         color:'#777777', 
-//         metalness:0.3,
-//         roughness:0.4,
-//         side:DoubleSide
-//     })
-// )
-// floor.rotation.x = -Math.PI * 0.5
-// scene.add(floor)
-
-// const floorShape = new CANNON.Plane()
-// const floorBody = new CANNON.Body()
-// floorBody.mass=0
-// floorBody.addShape(floorShape)
-// floorBody.mass = defaultMaterial
-// floorBody.quaternion.setFromAxisAngle(
-//     new CANNON.Vec3(-1, 0, 0), 
-//     Math.PI * 0.5 + 0.01
-// )
-// world.addBody(floorBody)
-
-// const floor = new THREE.Mesh(
-//     new THREE.PlaneBufferGeometry(5, 5), 
-//     new THREE.MeshStandardMaterial({
-//         color:'#777777', 
-//         metalness:0.5, 
-//         roughness:0.4,
-//         side:DoubleSide
-//     })
-// )
-// floor.rotation.x = -Math.PI * 0.5
-//scene.add(floor)
+const material1 = new THREE.ShaderMaterial({
+    side: THREE.DoubleSide,
+    extensions:{
+        derivatives:"#extension GL_OES_standard_derivatives : enable"
+    },
+    fragmentShader: fragment1,
+    vertexShader: vertex,
+    uniforms: {
+        time: {
+            type: "f",
+            value: 0
+        },
+        mouse: {
+            value: 0
+        },
+        landscape: {
+            value: t
+        },
+        resolution:{type:"v4", value: new THREE.Vector4()},
+        uvRate1:{
+            value: new THREE.Vector2(1,1)
+        }
+    }
+})
 
 //Sizes 
 const sizes = {
-    width:window.innerWidth,
-    height:window.innerHeight
+    width: window.innerWidth,
+    height: window.innerHeight
 }
 
 //Resize 
-window.addEventListener('resize', ()=>{
+window.addEventListener('resize', () => {
     sizes.width = window.innerWidth
     sizes.height = window.innerHeight
 
     //Update Camera 
-    camera.aspect = sizes.width /sizes.height
+    camera.aspect = sizes.width / sizes.height
     camera.updateProjectionMatrix()
 
     //Update renderer 
@@ -131,68 +124,74 @@ window.addEventListener('resize', ()=>{
  * Camera
  */
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
-camera.position.set(-8, 2, 1)
+camera.position.set(-2, 0, 1)
 scene.add(camera)
 
 //Controls 
 const controls = new OrbitControls(camera, canvas)
 controls.enableDamping = true
 
+
 /**
  * Renderer
-*/
+ */
 
 const renderer = new THREE.WebGLRenderer({
-    canvas:canvas,
-    alpha:true
+    canvas: canvas,
+    alpha: true
 })
 renderer.setSize(sizes.width, sizes.height)
-renderer.setClearColor(0xebebeb, 0);
 
+//Точки на экране
+let  composer = new EffectComposer( renderer );
+composer.addPass( new RenderPass( scene, camera ) );
+
+let customPass = new ShaderPass( PostProcessing );
+customPass.uniforms[ "resolution" ].value = new THREE.Vector2( window.innerWidth, window.innerHeight );
+customPass.uniforms[ "resolution" ].value.multiplyScalar( window.devicePixelRatio );
+composer.addPass( customPass );
+
+composer.setSize(sizes.width, sizes.height)
+renderer.setClearColor(0xebebeb, 0);
+composer.render()
 
 
 /**
  * Light
  */
-const ambientLight= new THREE.AmbientLight(0xffffff, 0.8)
+const ambientLight = new THREE.AmbientLight(0xffffff, 0.8)
 scene.add(ambientLight)
 
-const pointerLight = new THREE.PointLight(0x00ff00, 4, 20 )
-pointerLight.position.set(2, 8, 2 )
+const pointerLight = new THREE.PointLight(0x00ff00, 4, 20)
+pointerLight.position.set(2, 8, 2)
 scene.add(pointerLight)
 
 
-// const customUniforms = {
-//     uTime : {value:0}
-// }   
+let geometry1 =
+    new THREE.IcosahedronBufferGeometry(1.1, 1)
 
-// const createSphere = (radius, position)=>{
-    
-//       const sphereGeometry =  new THREE.CylinderGeometry( 2, 2, 2 );
-//       const sphereMaterial = new THREE.MeshStandardMaterial({
-//             emissive:0xbd4be3,
-//             emissiveIntensity:0.5,
-//             roughness:0.61, 
-//             metalness:0.21, 
-//             side: DoubleSide, 
-//             color:'black'
-//         })
-//     }
-
-//scene.add(createSphere)
-
-const geometry = new THREE.Mesh(
-    new THREE.IcosahedronGeometry(2, 1),
-    // new THREE.MeshStandardMaterial({
-    //     wireframe:true
-    // })
-    material
-)
-
-geometry.position.set(1, 2, 1)
+let length = geometry1.attributes.position.array.length
 
 
-scene.add(geometry)
+let bary = []
+
+for (let i = 0; i < length/3; i++) {
+    bary.push(0, 0, 1, 0, 1, 0, 0, 0, 1)
+}
+
+
+let aBary = new Float32Array(bary)
+
+geometry1.setAttribute('aBary', new THREE.BufferAttribute(aBary, 3),)
+
+let ico = new THREE.Mesh(geometry1, material)
+let icoLines = new THREE.Mesh(geometry1, material1)
+
+ico.position.set(0, 0, 0)
+
+scene.add(ico)
+scene.add(icoLines)
+
 
 /**
  * Animate
@@ -200,18 +199,30 @@ scene.add(geometry)
 const clock = new THREE.Clock()
 let oldElapsedTime = 0
 
-const tick = () =>
-{
+
+const tick = () => {
     const elapsedTime = clock.getElapsedTime()
-    const deltaTime = elapsedTime  - oldElapsedTime
+    const deltaTime = elapsedTime - oldElapsedTime
     oldElapsedTime = elapsedTime
 
-    //world.step(1/60, deltaTime, 3)
-
+    ico.rotation.x = elapsedTime / 20
+    icoLines.rotation.x = elapsedTime / 20
+    
     //Update controls
-    controls.update()
+    mouse -=(mouse - speed) * 0.05;
+    speed*=0.99
 
-    renderer.render(scene, camera)
+    controls.update()
+   
+    customPass.uniforms.howmuchrgbshifticanhas.value = mouse/5
+    customPass.uniforms.time.value = elapsedTime
+    material1.uniforms.time.value = elapsedTime
+    material1.uniforms.mouse.value = mouse/5
+    material.uniforms.time.value = elapsedTime
+    material.uniforms.mouse.value = mouse/5
+   
+    //renderer.render(scene, camera)
+    composer.render()
 
     //call tick again on the next frame 
     window.requestAnimationFrame(tick)
